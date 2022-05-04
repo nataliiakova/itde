6830c27557c5eb1d091beffd29d57390cb7bee2d
nataliiakova <100089843+nataliiakova@users.noreply.github.com>
2022-05-04 15:30:54 +0300

    Update week11code.js

--- week11code.js
+++ week11code.js
@@ -31,15 +31,27 @@ function init()
 	scene.add(light);
         
 	var cylgeometry = new THREE.CylinderGeometry(3, 3, 7, 7);
-	var cylmaterial = new THREE.MeshLambertMaterial({map: loader.load('fandom.jpeg')}); //=> {
-		//var cylmaterial = new THREE.MeshLambertMaterial({
-		//	map: texture,
-		//});
+	loader.load('fandom.jpeg', (texture) => {
+		var cylmaterial = new THREE.MeshLambertMaterial({
+			map: texture,
+		});
 		var cylmesh = new THREE.Mesh(cylgeometry, cylmaterial);
 		cylmesh.position.set(0.9, -5, -6);
 	    scene.add(cylmesh);
 	});
-
+var cubematerials = [
+	new THREE.MeshBasicMaterial({map: loader.load('https://farm1.staticflickr.com/854/42936929215_efa87c8a9a_b.jpg')}),
+new THREE.MeshBasicMaterial({map: loader.load('https://farm1.staticflickr.com/514/18832759790_bed1aeece8_b.jpg')}),
+new THREE.MeshBasicMaterial({map: loader.load('https://farm7.staticflickr.com/6140/5934453114_3675350a78_b.jpg')}),
+new THREE.MeshBasicMaterial({map: loader.load('https://farm3.static.flickr.com/2342/2269094999_6ac65c0947.jpg')}),
+new THREE.MeshBasicMaterial({map: loader.load('https://live.staticflickr.com/1757/41821018565_614db58ddc_b.jpg')}),
+new THREE.MeshBasicMaterial({map: loader.load('https://live.staticflickr.com/3103/2420240470_bc0bb7a260.jpg')}),
+];
+var cubegeometry = new THREE.CubeGeometry(3, 4, 3, 4);
+var cubemesh = new THREE.Mesh(cubegeometry, cubematerials);
+cubemesh.position.set(-1, -3, -2);
+scene.add(cubemesh);
+	
 	controls = new THREE.OrbitControls(camera,renderer.domElement);
 }
 
